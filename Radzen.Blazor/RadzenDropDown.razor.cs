08dda6b973747f2f35b64c1e18453cbee38cbcab
developertest02 <41734220+developertest02@users.noreply.github.com>
2022-08-10 07:47:15 -0400

    missing stuff

---
+++ Radzen.Blazor/RadzenDropDown.razor.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+    /// <summary>
+    /// RadzenDropDown component.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    /// <example>
+    /// <code>
+    /// &lt;RadzenDropDown @bind-Value=@customerID TValue="string" Data=@customers TextProperty="CompanyName" ValueProperty="CustomerID" Change=@(args => Console.WriteLine($"Selected CustomerID: {args}")) /&gt;
+    /// </code>
+    /// </example>
+    public partial class RadzenDropDown<TValue> : DropDownBase<TValue>
+    {
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is read only.
+        /// </summary>
+        /// <value><c>true</c> if is read only; otherwise, <c>false</c>.</value>
+        [Parameter]
+        public bool ReadOnly { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value template.
+        /// </summary>
+        /// <value>The value template.</value>
+        [Parameter]
+        public RenderFragment<dynamic> ValueTemplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether popup should open on focus. Set to <c>false</c> by default.
+        /// </summary>
+        /// <value><c>true</c> if popup should open on focus; otherwise, <c>false</c>.</value>
+        [Parameter]
+        public bool OpenOnFocus { get; set; }
+
+        private async Task OnFocus(Microsoft.AspNetCore.Components.Web.FocusEventArgs args)
+        {
+            if (OpenOnFocus)
+            {
+                await OpenPopup("Enter", false);
+            }
+        }
+
+        /// <summary>
+        /// Opens the popup.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="isFilter">if set to <c>true</c> [is filter].</param>
+        /// <param name="isFromClick">if set to <c>true</c> [is from click].</param>
+        protected override async System.Threading.Tasks.Task OpenPopup(string key = "ArrowDown", bool isFilter = false, bool isFromClick = false)
+        {
+            if (Disabled)
+                return;
+
+            await JSRuntime.InvokeVoidAsync(OpenOnFocus ? "Radzen.openPopup" : "Radzen.togglePopup", Element, PopupID, true);
+            await JSRuntime.InvokeVoidAsync("Radzen.focusElement", isFilter ? UniqueID : SearchID);
+
+            if (list != null)
+            {
+                await JSRuntime.InvokeVoidAsync("Radzen.selectListItem", search, list, selectedIndex);
+            }
+        }
+
+        internal override void RenderItem(RenderTreeBuilder builder, object item)
+        {
+            builder.OpenComponent(0, typeof(RadzenDropDownItem<TValue>));
+            builder.AddAttribute(1, "DropDown", this);
+            builder.AddAttribute(2, "Item", item);
+            if (DisabledProperty!=null)
+                builder.AddAttribute(3, "Disabled", PropertyAccess.GetItemOrValueFromProperty(item, DisabledProperty));
+
+            builder.SetKey(GetKey(item));
+            builder.CloseComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the number of maximum selected labels.
+        /// </summary>
+        /// <value>The number of maximum selected labels.</value>
+        [Parameter]
+        public int MaxSelectedLabels { get; set; } = 4;
+
+        /// <summary>
+        /// Gets or sets the selected items text.
+        /// </summary>
+        /// <value>The selected items text.</value>
+        [Parameter]
+        public string SelectedItemsText { get; set; } = "items selected";
+
+        /// <summary>
+        /// Gets or sets the select all text.
+        /// </summary>
+        /// <value>The select all text.</value>
+        [Parameter]
+        public string SelectAllText { get; set; }
+
+        private bool visibleChanged = false;
+        private bool disabledChanged = false;
+        private bool firstRender = true;
+
+        /// <inheritdoc />
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            visibleChanged = parameters.DidParameterChange(nameof(Visible), Visible);
+            disabledChanged = parameters.DidParameterChange(nameof(Disabled), Disabled);
+
+            await base.SetParametersAsync(parameters);
+
+            if (visibleChanged && !firstRender)
+            {
+                if (Visible == false)
+                {
+                    Dispose();
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            this.firstRender = firstRender;
+
+            if (firstRender || visibleChanged || disabledChanged)
+            {
+                visibleChanged = false;
+                disabledChanged = false;
+
+                if (Visible)
+                {
+                    bool reload = false;
+                    if (LoadData.HasDelegate && Data == null)
+                    {
+                        await LoadData.InvokeAsync(await GetLoadDataArgs());
+                        reload = true;
+                    }
+
+                    if (!Disabled)
+                    {
+                        await JSRuntime.InvokeVoidAsync("Radzen.preventArrows", Element);
+                        reload = true;
+                    }
+
+                    if (reload)
+                    {
+                        StateHasChanged();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when item is selected.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="isFromKey">if set to <c>true</c> [is from key].</param>
+        protected override async System.Threading.Tasks.Task OnSelectItem(object item, bool isFromKey = false)
+        {
+            if (!ReadOnly)
+            {
+                if (!Multiple && !isFromKey)
+                {
+                    await JSRuntime.InvokeVoidAsync("Radzen.closePopup", PopupID);
+                }
+
+                await SelectItem(item);
+            }
+        }
+
+        internal async System.Threading.Tasks.Task OnSelectItemInternal(object item, bool isFromKey = false)
+        {
+            await OnSelectItem(item, isFromKey);
+        }
+
+        /// <inheritdoc />
+        protected override string GetComponentCssClass()
+        {
+            return GetClassList("rz-dropdown").Add("rz-clear", AllowClear).ToString();
+        }
+
+        /// <inheritdoc />
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            if (IsJSRuntimeAvailable)
+            {
+                JSRuntime.InvokeVoidAsync("Radzen.destroyPopup", PopupID);
+            }
+        }
+
+        internal async System.Threading.Tasks.Task ClosePopup()
+        {
+            await JSRuntime.InvokeVoidAsync("Radzen.closePopup", PopupID);
+        }
+    }
+}
